f2fc9e1a8123efa23ae2ac96ce45ba888bd04b99
NastyaBondy <nastya.bonadysenko.01@mail.ru>
2022-09-18 19:01:23 +0300

    update

---
+++ LogicTrans/LogicTrans/app.js
@@ -0,0 +1,46 @@
+﻿angular.module('myapp', [])
+    .directive('ngTree', function () {
+        return {
+            restrict: 'E',
+            transclude: true,
+
+            controller: function ($scope) {
+
+                $scope.showVacancies = function (item) {
+                    item.active = !item.active;
+                };
+
+                $scope.items = [
+                    {
+                        label: "Основные требования",
+                        requirements: [
+                            { requirement: "наличие водительского удостоверения категории Е" },
+                            { requirement: "опыт работы по категории на тягаче с полуприцепом не менее 3 лет" },
+                            { requirement: "не менее 10-ти самостоятельных рейсов в Европу" },
+                            { requirement: "удостоверение водителя - международника" }
+                        ]
+                    },
+                    {
+                        label: "Условия работы",
+                        conditions: [
+                            { condition: "автопоезда DAF, MAN+ полуприцепы Schmitz, тенты и тралы класса Euro-5" },
+                            { condition: "обслуживание и ремонт техники в ЕС на СТО" },
+                            { condition: "стоянка и офис на одной территории" },
+                            { condition: "своевременная выплата заработной платы" },
+                            { condition: "отсутствие простоев без загрузок" }
+                        ]
+                    },
+                    {
+                        label: "Дополнительная информация",
+                        additional_info: [
+                            { info: "оптимальный возраст от 25 лет" },
+                            { info: "возможны командировки более 3-х дней" },
+                            { info: "ученики очных форм обучения не рассматриваются" }
+                        ]
+                    }
+                ];
+            },
+            templateUrl: 'treeview.html'
+        };
+    });
+
---
+++ LogicTrans/LogicTrans/script.js
@@ -0,0 +1,43 @@
+﻿angular.module('myapp', []);
+angular.module('myapp').controller('cntrl_1', function ($scope, $rootScope) {
+    $rootScope.btn_status = true;
+    $rootScope.mainCtrlBtnLabel = "Узнать больше"
+    $rootScope.btn_status_msg = "Сейчас недоступно";
+    $scope.triggerBroadcast = function () {
+        $scope.$broadcast('enable_Btns', { "btnEnable": false, "message": "Узнать подробнее" });
+    }
+    $scope.$on('btn_enabled', function (event, data) {
+        $rootScope.mainCtrlBtnLabel = data.main_ctrl_msg;
+    });
+});
+angular.module('myapp').controller('cntrl_2', function ($scope, $rootScope) {
+    $scope.$on('enable_Btns', function (event, data) {
+        console.log(data.btnEnable);
+        $rootScope.btn_status = data.btnEnable;
+        $rootScope.btn_status_msg = data.message;
+    })
+});
+angular.module('myapp').controller('cntrl_3', function ($scope, $rootScope) {
+    $scope.$on('enable_Btns', function (event, data) {
+        console.log(data.btnEnable);
+        $rootScope.btn_status = data.btnEnable;
+        $rootScope.btn_status_msg = data.message;
+    })
+});
+angular.module('myapp').controller('cntrl_4', function ($scope, $rootScope) {
+    $scope.$on('enable_Btns', function (event, data) {
+        console.log(data.btnEnable);
+        $rootScope.btn_status = data.btnEnable;
+        $rootScope.btn_status_msg = data.message;
+    })
+});
+angular.module('myapp').controller('cntrl_5', function ($scope, $rootScope, $timeout) {
+    $scope.$on('enable_Btns', function (event, data) {
+        console.log(data.btnEnable);
+        $rootScope.btn_status = data.btnEnable;
+        $rootScope.btn_status_msg = data.message;
+        $timeout(function () {
+            $scope.$emit('btn_enabled', { "main_ctrl_msg": "Выберите подходящую вам услугу" });
+        }, 1000)
+    });
+});
